6268e5417dfaf96817330b0c5df0b0de1bf3075f
vasya_tovt <vasya_tovt@mail.ru>
2019-02-23 15:59:07 +0200

    Core refactorings

--- BlueberryOpenTK/Core.cs
+++ BlueberryOpenTK/Core.cs
@@ -1,33 +1,53 @@
 ﻿using Blueberry;
 using OpenTK;
 using OpenTK.Graphics.OpenGL4;
+using OpenTK.Input;
 using System;
 using System.Threading;
 
-namespace BlueberryOpenTK
+namespace Blueberry
 {
-    public class Core : GameWindow
+    public partial class Core
     {
         public readonly Graphics graphics;
-
-        private Scene _current, _next;
-
         public Col BackgroundColor { get; set; } = Col.Black;
-        
+        public bool ForceSceneChange { get; set; } = false;
+
         public Scene Scene
         {
             get
             {
-                return _current;
+                return currentScene;
             }
             set
             {
-                _next = value;
+                nextScene = value;
+                if (ForceSceneChange)
+                    ChangeScene();
             }
         }
 
+        public int Width { get => window.Width; set => window.Width = value; }
+        public int Height { get => window.Height; set => window.Height = value; }
+
+        public event EventHandler<KeyEventArgs> KeyDown;
+        public event EventHandler<KeyEventArgs> KeyUp;
+        public event EventHandler<KeyPressEventArgs> KeyPress;
+        public event EventHandler<MouseButtonEventArgs> MouseUp;
+        public event EventHandler<MouseButtonEventArgs> MouseDown;
+        public event EventHandler<MouseMoveEventArgs> MouseMove;
+        public event EventHandler<ScrollEventArgs> MouseWheel;
+
+        protected Scene currentScene, nextScene;
+        protected OpenTKWindow window;
+
         public Core()
         {
+            window = new OpenTKWindow(this)
+            {
+                VSync = VSyncMode.Adaptive
+            };
+
             graphics = new Graphics();
             Screen.Width = Width;
             Screen.Height = Height;
@@ -35,62 +55,129 @@ namespace BlueberryOpenTK
             Input.Initialize(this);
         }
 
-        protected override void OnUpdateFrame(FrameEventArgs e)
+        public void Run(int rate)
         {
-            base.OnUpdateFrame(e);
-
-            if (_next != null)
-            {
-                if (_current != null)
-                    _current.End();
-                _current = _next;
-                _next = null;
-
-                _current.graphics = graphics;
-                _current.Initialize();
-                _current.OnResize(Width, Height);
-                _current.Begin();
-            }
-            if (_current != null)
-                _current.Update((float)e.Time);
+            window.SleepTime = 1000 / rate;
+            window.Run(rate);
         }
-        
-        protected override void OnRenderFrame(FrameEventArgs e)
-        {
-            base.OnRenderFrame(e);
 
-            if (Render.NeedsRequest && !Render.Requested)
+        public virtual void RenderFrame()
+        {
+            graphics.Projection = Screen.DefaultProjection;
+            graphics.Transform = Screen.DefaultTransform;
+            currentScene?.Render();
+            graphics.Flush();
+        }
+
+        public void Run(int updateRate, int renderRate)
+        {
+            window.SleepTime = 1000 / renderRate;
+            window.Run(updateRate, renderRate);
+        }
+
+        protected void ChangeScene()
+        {
+            if (nextScene != null)
             {
+                if (currentScene != null)
+                    currentScene.End();
+                currentScene = nextScene;
+                nextScene = null;
+
+                currentScene.graphics = graphics;
+                currentScene.Initialize();
+                currentScene.OnResize(Width, Height);
+                currentScene.Begin();
+            }
+        }
+
+        protected class OpenTKWindow : GameWindow
+        {
+            private Core core;
+
+            public int SleepTime { get; set; }
+
+            public OpenTKWindow(Core core)
+            {
+                this.core = core;
+            }
+
+            protected override void OnUpdateFrame(FrameEventArgs e)
+            {
+                base.OnUpdateFrame(e);
+
+                core.ChangeScene();
+
+                if (core.currentScene != null)
+                    core.currentScene.Update((float)e.Time);
+            }
+
+            protected override void OnRenderFrame(FrameEventArgs e)
+            {
+                base.OnRenderFrame(e);
+
+                if (Render.NeedsRequest && !Render.Requested)
+                {
+                    Thread.Sleep(SleepTime);
+                    return;
+                }
+
+                core.graphics.Clear(core.BackgroundColor);
+
+                if (core.currentScene != null)
+                {
+                    core.RenderFrame();
+                    Render.Complete();
+                }
+
+                SwapBuffers();
                 Thread.Sleep(16);
-                return;
             }
 
-            graphics.Clear(BackgroundColor);
-            
-            if (_current != null)
+            protected override void OnResize(EventArgs e)
             {
-                _current.Render();
-                Render.Complete();
+                base.OnResize(e);
+                GL.Viewport(0, 0, Width, Height);
+                Screen.Width = Width;
+                Screen.Height = Height;
+                Render.Request();
+                core.currentScene?.OnResize(Width, Height);
             }
 
-            SwapBuffers();
-            Thread.Sleep(16);
-        }
+            protected override void OnKeyUp(KeyboardKeyEventArgs e)
+            {
+                core.KeyUp?.Invoke(this, new KeyEventArgs((Key)e.Key, e.Shift, e.Control, e.Alt));
+            }
 
-        protected override void OnLoad(EventArgs e)
-        {
-            base.OnLoad(e);
-            GL.ClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-        }
+            protected override void OnKeyDown(KeyboardKeyEventArgs e)
+            {
+                core.KeyDown?.Invoke(this, new KeyEventArgs((Key)e.Key, e.Shift, e.Control, e.Alt));
+            }
 
-        protected override void OnResize(EventArgs e)
-        {
-            base.OnResize(e);
-            GL.Viewport(0, 0, Width, Height);
-            Screen.Width = Width;
-            Screen.Height = Height;
-            Render.Request();
-            _current?.OnResize(Width, Height);
+            protected override void OnKeyPress(OpenTK.KeyPressEventArgs e)
+            {
+                core.KeyPress?.Invoke(this, new KeyPressEventArgs(e.KeyChar));
+            }
+
+            protected override void OnMouseDown(OpenTK.Input.MouseButtonEventArgs e)
+            {
+                core.MouseDown?.Invoke(this, new MouseButtonEventArgs((MouseButton)e.Button, e.X, e.Y));
+            }
+
+            protected override void OnMouseUp(OpenTK.Input.MouseButtonEventArgs e)
+            {
+                core.MouseUp?.Invoke(this, new MouseButtonEventArgs((MouseButton)e.Button, e.X, e.Y));
+            }
+
+            protected override void OnMouseMove(OpenTK.Input.MouseMoveEventArgs e)
+            {
+                core.MouseMove?.Invoke(this, new MouseMoveEventArgs(e.X, e.Y));
+            }
+
+            protected override void OnMouseWheel(MouseWheelEventArgs e)
+            {
+                core.MouseWheel?.Invoke(this, new ScrollEventArgs(e.Mouse.Scroll.X, e.Mouse.Scroll.Y));
+            }
         }
     }
 }
